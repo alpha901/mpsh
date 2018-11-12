2016b336b753f3c1df37cee3cf7ac5501817410f
luoyaoyi@outlook.com <luoyaoyi@outlook.com>
2018-11-13 00:53:30 +0100

    modifié : main ( créer les fils) ajouté : fonction traitementCommande ( il y a une pb quand on passe le commande dans cette fonction)

--- mpsh.c
+++ mpsh.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 #define NB_COMMANDES 13
@@ -31,7 +34,7 @@ short commandeCorrecte(char *commande){
 			int caractereValide=0,i=0;
 			caractereCourant=mot[i];
 			while(caractereCourant!='\0'){
-				printf("%c ",caractereCourant);
+				// printf("%c ",caractereCourant);
 				if(!isalnum(caractereCourant)){
 					for(int j=0;j<CARACTERES_AUTORISES;j++){
 						if(caracteresAutorises[j]==caractereCourant){
@@ -44,20 +47,74 @@ short commandeCorrecte(char *commande){
 				}
 				caractereCourant=mot[++i];
 			}
-		}//else	
+		}//else
 		mot=strtok(NULL," ");
 	}//while
 	return 1;
 }
 
+//On traite la commande correcte
+void traitementCommande(char *commande){
+	printf("test : le commande est %s\n", commande);
+	char *cle;
+	int argc = 1;
+	cle =strtok(commande," "); // on parcours tous les mots de la commande séparés par des espaces
+	int pos = -1;
+	//pour localiser la commande en utilisant le tab nomsCommandes
+	for(int j=0;j<NB_COMMANDES;j++){
+		if(strcmp(nomsCommandes[j],cle)==0){
+			pos = j;
+			break;
+		}
+	}
+	// pour compter qu'il y a combien d'argument
+	while (cle != NULL)
+	{
+		cle = strtok(NULL, " ");
+		argc++;
+	}
+
+	// stocker les arguments de commande dans un tab de chaine de caratère
+	char * cmd[argc];
+	cle =strtok(commande," ");
+	cmd[0] = cle;
+	printf("test : cmd[0]%s\n",cmd[0]);
+	for (int i = 1; i < argc; i++) {
+		cmd[i] = strtok(NULL, "\0");
+		printf("test : cmd[%d]%s\n",i,cmd[i]);
+	}
+
+	// appliquer exec
+	if (pos>0) {
+		if (pos==6) {
+			printf("test : [%s]\n",cle );
+			execl("/bin/ls", "ls", cmd[1], (char *)0);
+		}
+	}
+
+}
+
 int main() {
   while(1){
     char *commande=readline("mon prompt>>");
-	printf("%s \n",commande);
-    printf("%d \n",commandeCorrecte(commande));
+		printf("%s\n",commande);
+
+		if(!commandeCorrecte(commande)){
+			// si la commande n'est pas correcte, on affiche error et refait la boucle.
+			printf("Votre commande n'est pas correcte.\n");
+		}else{
+			// sinon la commande est correcte, on la traite.
+			int pid = fork();//on crée des fils
+			if (pid<0) {
+				printf("Échec");
+				exit(1);
+			}else if (pid==0){
+				traitementCommande(commande);
+			}else{
+				waitpid(pid,NULL,0);// le père attend le fils
+			}
+		}
     free(commande);
   }
   return 0;
 }
-
-
